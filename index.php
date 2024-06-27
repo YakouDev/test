3e95aa8a0f8b1dbb218468734e94602ed2aca5d8
Yakou Developer <yakoudev@gmail.com>
2024-06-27 18:54:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -11,34 +11,37 @@ function http_get($url)
     curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
     curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($im, CURLOPT_HEADER, 0);
-    return curl_exec($im);
+    $response = curl_exec($im);
     curl_close($im);
+    return $response;
 }
+
 $domains = http_get("https://otx.alienvault.com/api/v1/indicators/IPv4/$host/passive_dns");
 $data = json_decode($domains, true);
 
-print_r($domains);
-
-foreach ($data['passive_dns'] as $key) {
-    $domain[] = $key['hostname'];
+$domain = [];
+if (isset($data['passive_dns']) && is_array($data['passive_dns'])) {
+    foreach ($data['passive_dns'] as $key) {
+        $domain[] = $key['hostname'];
+    }
 }
 
-if(empty($host)) {
-$status = array(
-"status" => 400,
-"msg" => "Bad Requests"
-);
-} elseif (empty($domainList)) {
-$status = array(
-"status" => 404,
-"msg" => "No Domain found"
-);
+if (empty($host)) {
+    $status = array(
+        "status" => 400,
+        "msg" => "Bad Requests"
+    );
+} elseif (empty($domain)) {
+    $status = array(
+        "status" => 404,
+        "msg" => "No Domain found"
+    );
 } else {
-$status = array(
-"status" => 200,
-"msg" => "Domain found",
-"domain" => $domain
-);
+    $status = array(
+        "status" => 200,
+        "msg" => "Domain found",
+        "domain" => $domain
+    );
 }
 echo json_encode($status, JSON_PRETTY_PRINT);
 ?>
